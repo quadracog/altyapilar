ec868048ce72b4731fa4d8ae121141d6e6be6d05
quadracog <94186575+quadracog@users.noreply.github.com>
2021-11-19 07:26:44 +0300

    Update sayfalireactionv11.js

--- altyapılar/sayfalireactionv11.js
+++ altyapılar/sayfalireactionv11.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js')
 const sayfaliab = require('discord.js-pagination')
 module.exports.run = async (client, message, args) => {
 
-    const sayfa1 = new Discord.MessageEmbed()
+    const sayfa1 = new Discord.RichEmbed()
     sayfa1.setTitle('Bu birinci')
 
-    const sayfa2 = new Discord.MessageEmbed()
+    const sayfa2 = new Discord.RichEmbed()
     sayfa2.setTitle('Bu fsbghfgjn')
 
-    const sayfa3 = new Discord.MessageEmbed()
+    const sayfa3 = new Discord.RichEmbed()
     sayfa3.setTitle('Bu fsjsgljfdsfhfskgjhfsghsfjkgh')
     const sayfalar = [
         sayfa1,
